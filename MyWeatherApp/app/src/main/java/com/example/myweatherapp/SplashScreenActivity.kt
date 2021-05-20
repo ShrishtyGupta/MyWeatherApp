79bd3f7b72b539eda4cae1b4ff6d90ef2d9e3aee
ShrishtyG <gupta.shrishty@gmail.com>
2021-05-20 19:10:47 +0530

    'SplashScreenAnimation'

--- MyWeatherApp/app/src/main/java/com/example/myweatherapp/SplashScreenActivity.kt
+++ MyWeatherApp/app/src/main/java/com/example/myweatherapp/SplashScreenActivity.kt
@@ -1,9 +1,14 @@
 package com.example.myweatherapp
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -13,6 +18,22 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val ta=AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val ba1=AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        val ba2 =AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        val al = findViewById<ImageView>(R.id.topImageView)
+        val bt = findViewById<TextView>(R.id.bottomText)
+        val bt1 = findViewById<TextView>(R.id.bottomTextView)
+
+        al.startAnimation(ta)
+        bt.startAnimation(ba1)
+        bt1.startAnimation(ba2)
+
+
+
+
+
+
         handler=Handler()
         handler.postDelayed({
 
@@ -20,5 +41,9 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         },2000)
+
+
+
+
     }
 }
